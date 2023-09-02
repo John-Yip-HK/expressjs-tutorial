588678096a0928368b3cd53c33e6a336f892ffe9
John-Yip-HK <yip4work@gmail.com>
2023-09-02 17:19:59 +0800

    3: POST requests

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+// Register a middleware to parse POST request body properly.
+// Middleware - a function that is invoked in the middle of 2 main functionalities.
+app.use(express.json());
+// Parse URL-encoded data.
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
   console.log(`Running Express Server on Port ${PORT}`);
 });
 
-app.get('/groceries', (req, res) => {
-  res.send([
+const groceryList = [
     {
       item: 'milk',
       quentity: 2,
@@ -17,5 +22,19 @@ app.get('/groceries', (req, res) => {
       item: 'cereal',
       quentity: 1,
     },
-  ]);
+  ]
+
+app.get('/groceries', (req, res) => {
+  res.send(groceryList);
+});
+
+app.post('/groceries', (req, res) => {
+  console.log(req.body);
+
+  groceryList.push(req.body);
+
+  // Deprecated
+  // res.send(201);
+
+  res.sendStatus(201);
 });
