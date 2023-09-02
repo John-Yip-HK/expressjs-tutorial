d91fe138a25f399721e91afd6c84eb9142bbd251
John-Yip-HK <yip4work@gmail.com>
2023-09-02 16:33:40 +0800

    2: GET requests

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Running Express Server on Port ${PORT}`);
 });
+
+app.get('/groceries', (req, res) => {
+  res.send([
+    {
+      item: 'milk',
+      quentity: 2,
+    },
+    {
+      item: 'cereal',
+      quentity: 1,
+    },
+  ]);
+});
